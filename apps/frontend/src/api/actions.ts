72ba2666ca877ce0ec5ddd6367082875bbc099de
Vishal Kashi <dev.vishalkashi@gmail.com>
2024-08-11 11:37:16 +0530

    fix: build errors

--- apps/frontend/src/api/actions.ts
+++ apps/frontend/src/api/actions.ts
@@ -1,5 +1,5 @@
 import { handleErrorResponse, in_dev_env } from "@/lib/helpers";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 const base_url = in_dev_env
   ? "http://localhost:3001"
@@ -10,7 +10,7 @@ export const getSeedPhrase = async () => {
     const response = await axios.get(`${base_url}/wallet/seed-phrase`);
     return response;
   } catch (error) {
-    handleErrorResponse(error);
+    if (error instanceof AxiosError) handleErrorResponse(error);
   }
 };
 
@@ -19,6 +19,6 @@ export const addNewWallet = async (payload: {}) => {
     const response = await axios.post(`${base_url}/wallet/get-wallet`, payload);
     return response;
   } catch (error) {
-    handleErrorResponse(error);
+    if (error instanceof AxiosError) handleErrorResponse(error);
   }
 };
