3a654892d14cb47cfa8bf29f810b403f13ada2e1
Vishal Kashi <dev.vishalkashi@gmail.com>
2024-08-11 12:42:22 +0530

    feat: add copy to clipboard feature

--- apps/frontend/src/components/common/WalletCard.tsx
+++ apps/frontend/src/components/common/WalletCard.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 
 import { Card } from "@/components/ui/card";
+import { toast } from "sonner";
 
 export default function WalletCard(props: {
   public_key: string;
   network: string;
 }) {
   const { public_key, network } = props;
+
+  const copyAddressToClipboard = () => {
+    navigator.clipboard.writeText(public_key);
+    toast("", {
+      description: "Copied to clipboard",
+      duration: 1000,
+    });
+  }
   return (
-    <Card className="py-2 px-3 rounded-lg">
+    <Card className="py-2 px-3 rounded-lg cursor-pointer" onClick={copyAddressToClipboard}>
       <div>
         <p className="text-sm text-muted-foreground">{network}</p>
       </div>
