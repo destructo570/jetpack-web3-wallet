2746511a56a2f99e177feedcaf994591587923c1
Vishal Kashi <dev.vishalkashi@gmail.com>
2024-08-11 10:56:21 +0530

    feat: add rate limiter

--- apps/backend/src/routes/wallet.ts
+++ apps/backend/src/routes/wallet.ts
@@ -1,10 +1,20 @@
 import { generateMnemonic } from "bip39";
 import { Hono } from "hono";
+import { rateLimiter } from "hono-rate-limiter";
 import { getEthereumWallet } from "../lib/utils";
+
+const limiter = rateLimiter({
+  windowMs: 60 * 1000, // 1 minutes
+  limit: 10, // Limit each IP to 100 requests per `window` (here, per 1 minutes).
+  standardHeaders: "draft-6", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  keyGenerator: (c) => "<unique_key>", // Method to generate custom identifiers for clients.
+});
 
 const walletRoutes = new Hono<{
   Bindings: {};
 }>();
+
+walletRoutes.use(limiter);
 
 walletRoutes.get("/seed-phrase", async (c) => {
   const mnemonic = generateMnemonic();
