09dbabcd0f3a3cbd42be112e640fdee49bd716dc
Vishal Kashi <dev.vishalkashi@gmail.com>
2024-08-20 23:44:37 +0530

    refac: refactor code

--- apps/backend/src/lib/utils.ts
+++ apps/backend/src/lib/utils.ts
@@ -1,17 +1,4 @@
-import { HDNode } from "@ethersproject/hdnode";
 import { ETHEREUM, SOLANA } from "../constants/urls";
-
-export const getEthereumWallet = (wallet_index: number, mnemonic: string) => {
-  const derivation_path = `m/44'/501'/${wallet_index}'/0'`;
-  const hd_node = HDNode.fromMnemonic(mnemonic).derivePath(derivation_path);
-
-  const wallet = {
-    network: "Ethereum",
-    public_key: hd_node.address,
-  };
-
-  return wallet;
-};
 
 export const getSolanaUrl = (api_key:string, type:string="mainnet") => {
   return `${SOLANA[type]}/${api_key}`;
