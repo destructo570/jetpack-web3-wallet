2881cadbf9f1484181f229122bac1634029cbf96
Vishal Kashi <dev.vishalkashi@gmail.com>
2024-08-22 00:05:04 +0530

    fix: build errors

--- apps/backend/src/lib/utils.ts
+++ apps/backend/src/lib/utils.ts
@@ -1,9 +1,12 @@
 import { ETHEREUM, SOLANA } from "../constants/urls";
 
-export const getSolanaUrl = (api_key:string, type:string="mainnet") => {
+type solNetworkType = "mainnet" | "devnet";
+type ethNetworkType = "mainnet" | "sepolia" | "holesky";
+
+export const getSolanaUrl = (api_key:string, type:solNetworkType="mainnet") => {
   return `${SOLANA[type]}/${api_key}`;
 }
 
-export const getEthUrl = (api_key:string, type:string="mainnet") => {
+export const getEthUrl = (api_key:string, type:ethNetworkType="mainnet") => {
   return `${ETHEREUM[type]}/${api_key}`;
 }
