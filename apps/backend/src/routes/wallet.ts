7c63ec3bd30c4c81a7a49c391a808bb6151061f0
Vishal Kashi <dev.vishalkashi@gmail.com>
2024-09-03 19:32:38 +0530

    feat: add route to airdrop sol

--- apps/backend/src/routes/wallet.ts
+++ apps/backend/src/routes/wallet.ts
@@ -18,7 +18,7 @@ walletRoutes.post("/get-eth-balance", async (c) => {
 
     let url = getEthUrl(ALCHEMY_KEY, network_type);
     const response = await axios.post(url, payload);
-    
+
     return c.json(response?.data);
   } catch (err) {
     c.status(500);
@@ -49,24 +49,46 @@ walletRoutes.post("/get-sol-balance", async (c) => {
 walletRoutes.get("/get-sol-nfts", async (c) => {
   const { SHYFT_API_KEY } = env<{ SHYFT_API_KEY: string }>(c);
   try {
-    const { public_key, page, size} = c.req.query();
+    const { public_key, page, size } = c.req.query();
     let url = `https://api.shyft.to/sol/v2/nft/read_all`;
-    
+
     let payload = {
       params: {
         network: "mainnet-beta",
         address: public_key,
         page,
-        size
+        size,
       },
       headers: {
         "Content-Type": "application/json",
         "x-api-key": SHYFT_API_KEY,
       },
-    }
+    };
     const response = await axios.get(url, payload);
-    
+
     return c.json(response?.data?.result);
+  } catch (err) {
+    c.status(500);
+    return c.text("Internal server error");
+  }
+});
+
+walletRoutes.post("/airdrop-sol", async (c) => {
+  const { ALCHEMY_KEY } = env<{ ALCHEMY_KEY: string }>(c);
+  try {
+    const { public_key, amount } = (await c.req.json()) || {};
+
+    let payload = {
+      id: 1,
+      jsonrpc: "2.0",
+      method: "requestAirdrop",
+      params: [public_key, parseFloat(amount) * 1000000000],
+    };
+
+    let url = getSolanaUrl(ALCHEMY_KEY, "devnet");
+    const response = await axios.post(url, payload);
+
+    return c.json(response?.data);
   } catch (err) {
     c.status(500);
     return c.text("Internal server error");
